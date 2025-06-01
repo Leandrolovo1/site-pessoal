b0d51711e55756767b4a70794ea306e2a011918a
Leandrolovo1 <135177984+Leandrolovo1@users.noreply.github.com>
2025-06-01 20:19:02 -0300

    iniciando atividade

---
+++ atividades/func_obj_arr/scripts/atv01.js
@@ -0,0 +1,15 @@
+
+function ex01() {
+    
+    const form = document.querySelector('#form01')
+    const input = form.querySelector('input[name="in_01"]').value
+    alert(input)
+    form.reset()
+}
+
+function resolve01() {
+
+} 
+
+
+
